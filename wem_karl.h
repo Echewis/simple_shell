2f080c219336014b0de0db6bafd3cff242bcc13d
Wem-Tech <houseofwem1@gmail.com>
2023-09-11 15:13:56 +0100

    This is the header file

---
+++ wem_karl.h
@@ -0,0 +1,17 @@
+#ifndef WEM_KARL_H
+#define WEM_KARL_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
+void printit(const char *okwu);
+void show_prompt(void);
+void run_command(const char *instruct);
+void recieve_command(char *st_c, size_t how_big);
+
+#endif
