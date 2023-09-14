afc1ce68b682820a6780f75124e8763f320fd065
Karl <tekamkarl@gmail.com>
2023-09-14 20:27:45 +0200

    adding comments

--- give_output.c
+++ give_output.c
@@ -6,20 +6,20 @@
  */
 void run_command(char *instruct)
 {
-	pid_t proccess_id = fork();
+	pid_t proccess_id = fork(); /* create a child process*/
 
-	if (proccess_id == -1)
+	if (proccess_id == -1) /* if the child process creation failed */
 	{
 		perror("fork");
 		exit(EXIT_FAILURE);
 	}
-	else if (proccess_id == 0)
+	else if (proccess_id == 0) /* if the child process was created successfully*/
 	{
-		execlp(instruct, instruct,(char *)NULL);
+		execlp(instruct, instruct,(char *)NULL); /* Execute the command specified by `instruct` */
 		perror("");
 		exit(EXIT_FAILURE);
 	}
-	else
+	else /* Tell the parent process to wait for the child process to finish */
 	{
 		wait(NULL);
 	}
