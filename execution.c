a63f6fc7481b157c0cd63ac1de774449add1cf27
Karl <tekamkarl@gmail.com>
2023-09-15 12:28:27 +0200

    execute a command

---
+++ execution.c
@@ -0,0 +1,23 @@
+#include "wem_karl.h"
+
+/**
+ * exe_file - execute a command
+ * @instruct: command to execute
+*/
+void exe_file(char *instruct)
+{
+	char *path;
+	char **args;
+
+	args = split_string(instruct);
+
+	if (args)
+	{
+		path = args[0];
+		if (execve(path, args, NULL) == -1)
+		{
+			perror("Error");
+			exit(EXIT_FAILURE);
+		}
+	}
+}
