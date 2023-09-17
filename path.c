45018f89bd97e1996ad248cf2f9c03f77e01f525
Karl <tekamkarl@gmail.com>
2023-09-17 15:23:51 +0200

    give the full path to the given command

---
+++ path.c
@@ -0,0 +1,25 @@
+#include "wem_karl.h"
+
+/**
+ * get_path - give the full path to the given command
+ * @command: command typing by the user
+ * Return: the path or NULL if the path is not executable
+ */
+char *get_path(char *command)
+{
+	char cmd_path[150];
+	char *path = getenv("PATH");
+	char *directory = strtok(path, ":");
+
+	while (directory != NULL)
+	{
+		snprintf(cmd_path, sizeof(cmd_path), "%s/%s", directory, command);
+		if (access(cmd_path, X_OK) == 0)
+		{
+			return (_strdup(cmd_path));
+		}
+		directory = strtok(NULL, ":");
+	}
+
+	return (NULL);
+}
