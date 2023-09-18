85b4d285fe4ef2ca8cc69644adc35b7ee9f8aa94
Karl <tekamkarl@gmail.com>
2023-09-18 07:11:42 +0200

    copy in string into another

--- our_strcpy.c
+++ our_strcpy.c
@@ -1,13 +1,22 @@
+#include "wem_karl.h"
+
+/**
+ * our_strcpy - copy in string into another.
+ * @dest: destination string
+ * @src: source string
+ *
+ * Return: Nothing
+ */
 void *our_strcpy(char *dest, const char *src)
 {
-    char *original = dest;
+	char *original = dest;
 
-    while (*src != '\0')
-    {
-        *dest = *src;
-        dest++;
-        src++;
-    }
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
 
-    *dest = '\0';
+	*dest = '\0';
 }
