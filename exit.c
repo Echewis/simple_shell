65e661ea0abbb896c48feb6309a6bce1e29ca89c
Wem-Tech <houseofwem1@gmail.com>
2023-09-18 18:05:10 +0100

    This file will terminate the shell

--- exit.c
+++ exit.c
@@ -1,7 +1,29 @@
 #include "wem_karl.h"
 
-
-void exit_shell()
+/**
+ * exit_shell - This is exit function that terminates the programm
+ */
+void exit_shell(void)
 {
-	exit(EXIT_SUCCESS);
+	char inst[150];
+	while (1)
+	{
+	fgets(inst, sizeof(inst), stdin);
+	inst[strcspn(inst, "\n")] == '\0';
+
+		if (strcmp(inst, "exit") == 0)
+		{
+			printit("Exiting...");
+			exit(EXIT_SUCCESS);
+		}
+	}
 }
+/*
+
+void exit_s(const char *s)
+{
+	if (strcmp(s, "exit") == 0)
+	{
+		exit(0);
+	}
+}*/
