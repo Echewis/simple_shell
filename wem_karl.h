2564f9c1f00825a25570d1e829c11792b16ac130
Karl <tekamkarl@gmail.com>
2023-09-18 06:57:35 +0200

    header file

--- wem_karl.h
+++ wem_karl.h
@@ -10,13 +10,18 @@
 #include <sys/wait.h>
 #include <sys/types.h>
 
+extern char **environ;
 void printit(const char *okwu);
 void show_prompt(void);
 void run_command(char *instruct);
 void recieve_command(char *buffer, size_t buffer_size);
 char **split_string(char *string);
 void exe_file(char *instruct);
-char* get_path(char* command);
-char *_strdup(char *str);
+char *get_path(char *command);
+char *our_getenv(char *env_name);
+char *our_strdup(char *str);
+void *our_strcpy(char *dest, const char *src);
+int our_strncmp(char *str1, char *str2, size_t n); 
+int our_strlen(char *s);
 
 #endif
