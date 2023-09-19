de2ca838524170969ef8d53cd848c4c7a6d3088b
Wem-Tech <houseofwem1@gmail.com>
2023-09-19 17:42:45 +0100

    This file receives imputs

--- rec_inputz.c
+++ rec_inputz.c
@@ -6,13 +6,9 @@
  */
 void recieve_command(char *buffer, size_t buffer_size)
 {
-/*	ssize_t read;*/
-/*	size_t lenght = 0;*/
 	/* reads at most buffer_size -1 and store it in buffer*/
 	while (fgets(buffer, buffer_size, stdin) == NULL)
-/*	while ((read = getline(&buffer, &lenght, stdin)) != -1)*/
 	{
-
 		if (feof(stdin)) /* if the end of input has been reached */
 		{
 			printit("\n");
@@ -23,6 +19,7 @@ void recieve_command(char *buffer, size_t buffer_size)
 			printit("Error, Try Again\n");
 			exit(EXIT_SUCCESS);
 		}
+
 	}
 	buffer[strcspn(buffer, "\n")] = '\0'; /* terminate additionnal line */
 }
